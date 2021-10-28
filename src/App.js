c7852c10df3f917176947002f8a91d3f3173ff37
Vladimir Palamov <84524771+moriarty15@users.noreply.github.com>
2021-10-28 11:54:15 +0800

    refactor

--- src/App.js
+++ src/App.js
@@ -1,68 +1,25 @@
-import React from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import ContactForm from './components/ContactForm';
-import ContactList from './components/ContactList';
-import Filter from './components/Filter/Filter';
-import Container from './components/Container';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends React.Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
-  // функция получения значения из любого инпута
-  handleAllInputChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
-  };
-
-  // метод добавления контакта в телефонную книгу
-  formSubmitHandler = data => {
-    // условие что контакт с таким именем есть в телефонной книге
-    if (this.state.contacts.some(e => e.name.includes(data.name))) {
-      alert(`${data.name} is already in contacts`);
-      return;
-    }
-    // объект с именем и номером телефона для пуша в общий массив контактов
-    const objd = {
-      name: data.name,
-      number: data.number,
-      id: uuidv4(),
-    };
-    this.setState({ contacts: [objd, ...this.state.contacts] });
-  };
-  // метод удаления контакта из телефонной книги
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
-  };
-
-  // вынес фильтр в функцию
-  getVisibleContacts = () => {
-    const { filter, contacts } = this.state;
-    const normalizeFilter = filter.toLowerCase();
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizeFilter),
-    );
-  };
-
-  render() {
-    const { filter } = this.state;
-    const visibleContacts = this.getVisibleContacts();
-    return (
-      <Container>
-        <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.formSubmitHandler} />
-        <h2>Contacts</h2>
-        <Filter filter={filter} onChange={this.handleAllInputChange} />
-        <ContactList
-          filter={visibleContacts}
-          onDeleteContacts={this.deleteContact}
-        />
-      </Container>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
